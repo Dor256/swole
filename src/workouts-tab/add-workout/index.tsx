451a3fe00a1341d2a6bdfeb2681461be4a5970bd
Dor256 <dorb@wix.com>
2021-08-03 23:49:49 +0300

    upgrade to react navigation 6 + add workout modal

--- src/workouts-tab/add-workout/index.tsx
+++ src/workouts-tab/add-workout/index.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import { Maybe } from '@unpacked/tool-belt';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
+import { Goal, IWorkout } from '../../common/api';
 import { Button } from '../../common/components/Button';
+import { Input } from '../../common/components/Input';
 import { View } from '../../common/components/Themed';
+import { useMaybeState } from '../../hooks/useMaybeState';
+import { Picker } from '@react-native-picker/picker';
 
 export type AddWorkoutProps = {
-  onAddWorkout(): void;
+  addWorkout(workout: Omit<IWorkout, 'id'>): Promise<void>;
 };
 
 export const AddWorkout: React.FC<AddWorkoutProps> = (props) => {
+  const [maybeName, setMaybeName] = useMaybeState<string>();
+  const [goal, setGoal] = useState<Goal>('strength');
+
+  function onAddWorkout() {
+    maybeName.inCaseOf({
+      Nothing: () => console.warn('Must enter name'),
+      Just: async (name) => {
+        await props.addWorkout({ name, goal });
+      }
+    });
+  }
+
+  function onNameChange(text: string) {
+    setMaybeName(Maybe.fromValue(text));
+  }
+
   return (
     <View style={styles.container}>
-      <Button onPress={props.onAddWorkout}>
+      <Input
+        value={maybeName.orElse('')}
+        onChangeText={onNameChange}
+      />
+      <Picker
+        style={styles.picker}
+        selectedValue={goal}
+        onValueChange={setGoal}
+      >
+        <Picker.Item label="Strength" value="strength" />
+        <Picker.Item label="Hypertrophy" value="hypertrophy" />
+      </Picker>
+      <Button onPress={onAddWorkout}>
         Add Workout
       </Button>
     </View>
@@ -19,6 +52,11 @@ export const AddWorkout: React.FC<AddWorkoutProps> = (props) => {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  picker: {
+    width: '45%'
   }
 });
